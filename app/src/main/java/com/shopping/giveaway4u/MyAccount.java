2b5e7afc3f4c861c9eafc2619c73f9569dd8bd9f
Pranav P <ppranav164@gmail.com>
2020-07-15 14:42:11 +0530

    New Activity for MyAccount

---
+++ app/src/main/java/com/shopping/giveaway4u/MyAccount.java
@@ -0,0 +1,14 @@
+package com.shopping.giveaway4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+    }
+}
